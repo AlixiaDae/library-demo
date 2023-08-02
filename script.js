92cf30d0f2930eeb93a55f287da0103f7b5f7de6
AlixiaDae <lizziejones1208@gmail.com>
2023-08-03 00:16:53 +0800

    Add Book and Library Classes

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+// Classes
+class Library {
+    constructor() {
+        this.books = []
+    }
+
+    getBooks() {
+        return this.books
+    }
+
+    addBook(newBook) {
+        if(this.books.find(book => book.name === newBook.name)) return
+
+        this.books.push(newBook)
+    }
+
+    deleteBook(bookName) {
+        this.books = this.books.filter(book => book.name !== bookName)
+    }
+}
+
+class Book {
+    constructor(name, author, pages, status) {
+        this.name = name
+        this.author = author
+        this.pages = pages
+        this.status = status
+    }
+
+    getName() {
+        return this.name
+    }
+
+}
+
+const library = new Library
+const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 320, "read")
+const silmarillion = new Book("The Silmarillion", "J.R.R. Tolkien", 454, "read")
+const babel = new Book("Babel, or the Necessity of Violence", "R.F. Kuang", 560, "read")
+
+library.addBook(hobbit)
+library.addBook(silmarillion)
+library.addBook(babel)
+
+
+// DOM Manipulator
+
 const main = document.querySelector("main")
 const screen = document.querySelector(".screen")
 
@@ -5,12 +52,22 @@ const addBookBtn = document.getElementById("addBooksBtn")
 const bookForm = document.querySelector("form")
 
 addBookBtn.addEventListener("click", showForm)
-addBookBtn.addEventListener("click", blur)
 
 function showForm() {
     bookForm.classList.toggle("show")
 }
 
-function blur() {
-    screen.classList.toggle("blur")
+function createCard(book) {
+    const card = document.createElement("div")
+    card.classList.add("card")
+
+    const title = document.createElement("p")
+    title.textContent = book.name
+
+    const author = document.createElement("p")
+    author.textContent = book.author
+
+    card.append(title, author)
+
+    return card
 }
