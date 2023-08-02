7f5892f0293ff77008bf3cd2071beed12e431d80
AlixiaDae <lizziejones1208@gmail.com>
2023-08-03 02:00:48 +0800

    Complete with localstorage

--- script.js
+++ script.js
@@ -33,7 +33,19 @@ class Book {
 
 }
 
-const library = new Library
+const library = Object.assign(
+    new Library(),
+    JSON.parse(localStorage.getItem("library"))
+)
+
+    library.getBooks().map(
+        book => Object.assign(
+        new Book(), book
+    )
+)
+
+
+
 const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 320, "read")
 const silmarillion = new Book("The Silmarillion", "J.R.R. Tolkien", 454, "read")
 const babel = new Book("Babel, or the Necessity of Violence", "R.F. Kuang", 560, "read")
@@ -41,6 +53,7 @@ const babel = new Book("Babel, or the Necessity of Violence", "R.F. Kuang", 560,
 library.addBook(hobbit)
 library.addBook(silmarillion)
 library.addBook(babel)
+localStorage.setItem("library", JSON.stringify(library))
 
 
 // DOM Manipulator
@@ -51,11 +64,46 @@ const shelf = document.querySelector(".shelf")
 const addBookBtn = document.getElementById("addBooksBtn")
 const bookForm = document.querySelector("form")
 
-addBookBtn.addEventListener("click", showForm)
+addBookBtn.addEventListener("click", () => {
+    showElement(bookForm)
+})
 
-function showForm() {
-    bookForm.classList.toggle("show")
+function showElement(element) {
+    element.classList.toggle("show")
 }
+
+const nameInput = document.getElementById("bookName")
+const authorInput = document.getElementById("authorName")
+const pageInput = document.getElementById("pageNumber")
+const radioBtns = document.querySelectorAll('input[name="status"]')
+
+const createBookBtn = document.getElementById("createBookBtn")
+
+createBookBtn.addEventListener("click", (e) => {
+    if(nameInput.value === "" || authorInput.value === "") {
+        return
+    } 
+
+    const bookName = nameInput.value
+    const authorName = authorInput.value
+    const pageNumber = pageInput.value
+    let status;
+
+    for (const radioBtn of radioBtns) {
+        if (radioBtn.checked) {
+            status = radioBtn.value
+        } else {
+            status = "unread"
+        }
+    }
+
+    const newBook = new Book(bookName, authorName, pageNumber, status)
+
+    library.addBook(newBook)
+    localStorage.setItem("library", JSON.stringify(library))
+    populateShelf(shelf)
+    showElement(bookForm)
+})
 
 function createCard(book) {
     const card = document.createElement("div")
@@ -73,11 +121,16 @@ function createCard(book) {
     return card
 }
 
+
 function populateShelf(parentElement) {
-    for(let book of library.getBooks()) {
-        parentElement.appendChild(createCard(book))
+    parentElement.innerHTML = ""
+    if(localStorage.getItem("library") === null) {
+        //
+    } else { 
+        for(let book of library.getBooks()) {
+            parentElement.appendChild(createCard(book))
+        }
     }
 }
-
 
 populateShelf(shelf)
